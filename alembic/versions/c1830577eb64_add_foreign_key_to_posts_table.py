59709618606a4a9eb938ed11731578b2e19fc802
Sean Lee <magne@Seanui-MacBookPro.local>
2022-10-31 19:41:24 +0900

    amend version file

--- alembic/versions/c1830577eb64_add_foreign_key_to_posts_table.py
+++ alembic/versions/c1830577eb64_add_foreign_key_to_posts_table.py
@@ -5,7 +5,6 @@ Revises: 5dbcb0c67215
 Create Date: 2022-10-27 18:02:00.538210
 
 """
-from tkinter import CASCADE
 from alembic import op
 import sqlalchemy as sa
 
@@ -20,7 +19,7 @@ depends_on = None
 def upgrade():
 	op.add_column('posts', sa.Column('owner_id', sa.Integer(), nullable=False))
 	op.create_foreign_key('post_users_fk', source_table='posts', referent_table="users", 
-	local_cols=['owner_id'], remote_cols=['id'], ondelete=CASCADE)
+	local_cols=['owner_id'], remote_cols=['id'], ondelete='CASCADE')
 	pass
 
 
